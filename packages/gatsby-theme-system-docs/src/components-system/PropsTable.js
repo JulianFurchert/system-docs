232da1b309631310fec799dd37be3d20bd6bcf52
jf <julian.furchert@raytion.com>
2020-03-27 15:10:34 +0100

    update propsTable

--- packages/gatsby-theme-system-docs/src/components-system/PropsTable.js
+++ packages/gatsby-theme-system-docs/src/components-system/PropsTable.js
@@ -4,6 +4,8 @@ import Code from './Code'
 
 export function PropsTable({ data, title = "Props" }) {
   const hasProps = Object.keys(data).length > 0;
+  const hasDescription = Object.entries(data).find(([, value]) => value.description) ? true : false
+  console.log(Object.entries(data).find(([, value]) => value.description))
 
   return (
     <Box
@@ -23,7 +25,7 @@ export function PropsTable({ data, title = "Props" }) {
                 <Styled.th>Prop</Styled.th>
                 <Styled.th>Type</Styled.th>
                 <Styled.th>Default</Styled.th>
-                {/* <Styled.th>Description</Styled.th> */}
+                {hasDescription && <Styled.th>Description</Styled.th>}
               </tr>
             </thead>
             <tbody>
@@ -45,9 +47,11 @@ export function PropsTable({ data, title = "Props" }) {
                         </Text>
                       )}
                     </Styled.td>
-                    {/* <Styled.td>
-                      <Text textColor="gray700">{value.description}</Text>
-                    </Styled.td> */}
+                    {hasDescription && (
+                      <Styled.td>
+                        <Text textColor="gray700">{value.description}</Text>
+                      </Styled.td> 
+                    )}
                   </tr>
                 );
               })}
